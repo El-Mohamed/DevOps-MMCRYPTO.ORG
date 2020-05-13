af6711d98ee4af26568b7a273615a5c9d4deb6f5
ElMoufid-Mohamed <elmoufid.mohamed@outlook.com>
2020-05-13 15:51:45 +0200

    Removed Unused

--- Ansible/StartJenkinsWithAnsible/startbuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Login
-java -jar jenkins-cli.jar -s  http://localhost:8080/ -auth devops:devops
-
-# Build
-java -jar jenkins-cli.jar -s  http://localhost:8080/ -auth devops:devops build "Devops Project"
-
