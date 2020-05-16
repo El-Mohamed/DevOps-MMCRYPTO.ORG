e7c3731bd3a3633506866249562481bfaf16afe0
ElMoufid-Mohamed <elmoufid.mohamed@outlook.com>
2020-05-16 21:13:34 +0200

    Add CheckMK Container Check To Ubuntu Playbook & Changed check_mk.sh

--- ContainerScripts/check-mk.sh
+++ ContainerScripts/check-mk.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker container run -dit -p 8081:5000 --ulimit nofile=1024 -v /root/sites:/omd/sites --name monitoring2 -v/etc/localtime:/etc/localtime:ro --restart always checkmk/check-mk-raw
+docker container run -dit -p 8081:5000 --ulimit nofile=1024 -v /root/sites:/omd/sites --name monitoring_container -v/etc/localtime:/etc/localtime:ro --restart always checkmk/check-mk-raw
